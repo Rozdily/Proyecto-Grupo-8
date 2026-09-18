51de7d77df4e1307dbcf22cc1dab34dce71d6125
Rozdily <humgergames123@gmail.com>
2026-09-18 22:23:27 +0000

    se cambió la ruta del css.

--- views/estudiante/panel.php
+++ views/estudiante/panel.php
@@ -36,7 +36,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tutorias - UPDS</title>
-    <link rel="stylesheet" href="../../assets/css/estudiante.css">
+    <link rel="stylesheet" href="../../assets/css/panel.css">
 </head>
 <body>
     <!-- Barra Nav Superior -->
